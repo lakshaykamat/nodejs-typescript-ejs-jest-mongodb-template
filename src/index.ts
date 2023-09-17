4cd2fb913205803c2e0cd357e977843cc1638f9d
Lakshay Kamat <lakshaykamat2048@gmail.com>
2023-09-17 13:42:59 +0530

    Index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Express  from "express";
 import exampleRoute from "./routes/exampleRoute.js";
 const app = Express()
@@ -10,17 +9,3 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 app.listen(PORT,()=>console.log("Server is listening on port 8080"))
-=======
-import express from 'express';
-
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello, TypeScript Node.js!');
-});
-
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
->>>>>>> main
